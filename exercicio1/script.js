ad1a00e2e008938c6b36a6de37d4d657144b01a0
wylchayer <wyldacruz@gmail.com>
2023-02-15 23:38:05 -0300

    Resolução exercícios de objetos

---
+++ exercicio1/script.js
@@ -0,0 +1,26 @@
+const sacolao = []
+
+// a)
+const fruta1 = {
+    nome: 'banana',
+    preco: 0.6,
+    disponibilidade: true
+}
+
+const fruta2 = {
+    nome: 'maça',
+    preco: 0.8,
+    disponibilidade: true
+}
+
+const fruta3 = {
+    nome: 'melão',
+    preco: 1,
+    disponibilidade: false
+}
+
+// b)
+sacolao.push(fruta1, fruta2, fruta3)
+
+// c)
+console.log(sacolao)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -13,3 +13,19 @@ const filme = {
 }
 
 // respostas aqui ⬇️
+
+// a)
+const primeiroAtorAtriz = filme.elenco[0]
+console.log(`Primeiro ator/atriz:`, primeiroAtorAtriz)
+
+// b)
+const ultimoAtorAtriz = filme.elenco[filme.elenco.length -1]
+console.log(`Último ator/atriz:`, ultimoAtorAtriz)
+
+// c)
+const todasTransmissoesHoje = filme.transmissoesHoje
+console.log(`Todas trasmissões de hoje:`, todasTransmissoesHoje)
+
+// d)
+const horarioTransmissaoCanalBrasil = filme.transmissoesHoje[1].horario
+console.log(`Horário de transmissão no Canal Brasil:`, horarioTransmissaoCanalBrasil)
